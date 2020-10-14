7b5a4d0f7f6d377f0667e76d2ff7f736ef5de2f7
Rene Saarsoo <nene@triin.net>
2020-10-14 22:27:38 +0300

    Format with purs

--- src/Repeats.purs
+++ src/Repeats.purs
@@ -1,58 +1,62 @@
 module Zwiftout.Repeats (detectRepeats, PlainOrRepeatedInterval(..), RepeatedInterval) where
 
 import Prelude
-
 import Zwiftout.Workout (Comment, Duration, Interval)
 import Data.List (List(..), all, concat, drop, length, scanl, take, takeWhile, zipWith, (:))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 
-type RepeatedInterval = {
-  times :: Int,
-  intervals :: List Interval,
-  comments :: List Comment
-}
+type RepeatedInterval
+  = { times :: Int
+    , intervals :: List Interval
+    , comments :: List Comment
+    }
 
-data PlainOrRepeatedInterval = Plain Interval | Repeated RepeatedInterval
+data PlainOrRepeatedInterval
+  = Plain Interval
+  | Repeated RepeatedInterval
 
 instance eqPlainOrRepeatedInterval :: Eq PlainOrRepeatedInterval where
-    eq (Plain a) (Plain b) = a == b
-    eq (Repeated a) (Repeated b) = a == b
-    eq (Plain a) (Repeated b) = false
-    eq (Repeated a) (Plain b) = false
+  eq (Plain a) (Plain b) = a == b
+  eq (Repeated a) (Repeated b) = a == b
+  eq (Plain a) (Repeated b) = false
+  eq (Repeated a) (Plain b) = false
 
 instance showPlainOrRepeatedInterval :: Show PlainOrRepeatedInterval where
-    show (Plain a) = "(Plain " <> show a <> ")"
-    show (Repeated a) = "(Repeated " <> show a <> ")"
+  show (Plain a) = "(Plain " <> show a <> ")"
+  show (Repeated a) = "(Repeated " <> show a <> ")"
 
 windowSize :: Int
 windowSize = 2
 
 detectRepeats :: List Interval -> List PlainOrRepeatedInterval
 detectRepeats intervals = case splitAt windowSize intervals of
-    Tuple Nil _ -> map Plain intervals
-    Tuple _ Nil -> map Plain intervals
-    Tuple (x:xs) ys -> case extractRepeatedInterval (x:xs) ys of
-        Just repeatedInterval -> Repeated repeatedInterval : detectRepeats (drop repetitionLength intervals) where
-            repetitionLength = repeatedInterval.times * windowSize
-        Nothing -> Plain x : detectRepeats (drop 1 intervals)
+  Tuple Nil _ -> map Plain intervals
+  Tuple _ Nil -> map Plain intervals
+  Tuple (x : xs) ys -> case extractRepeatedInterval (x : xs) ys of
+    Just repeatedInterval -> Repeated repeatedInterval : detectRepeats (drop repetitionLength intervals)
+      where
+      repetitionLength = repeatedInterval.times * windowSize
+    Nothing -> Plain x : detectRepeats (drop 1 intervals)
 
 extractRepeatedInterval :: List Interval -> List Interval -> Maybe RepeatedInterval
 extractRepeatedInterval reference xs = case countRepeats reference xs of
-    0 -> Nothing
-    n -> Just {
-        times: n + 1,
-        intervals: map stripComments reference,
-        comments: collectComments (concat (reference : take (n*windowSize) xs : Nil))
-    }
+  0 -> Nothing
+  n ->
+    Just
+      { times: n + 1
+      , intervals: map stripComments reference
+      , comments: collectComments (concat (reference : take (n * windowSize) xs : Nil))
+      }
 
 countRepeats :: List Interval -> List Interval -> Int
 countRepeats reference = chunk (length reference) >>> takeWhile (similar reference) >>> length
 
 similar :: List Interval -> List Interval -> Boolean
-similar xs ys = all identity (zipWith similar' xs ys) where
-    similar' :: Interval -> Interval -> Boolean
-    similar' x y = x.type == y.type && x.duration == y.duration && x.intensity == y.intensity && x.cadence == y.cadence
+similar xs ys = all identity (zipWith similar' xs ys)
+  where
+  similar' :: Interval -> Interval -> Boolean
+  similar' x y = x.type == y.type && x.duration == y.duration && x.intensity == y.intensity && x.cadence == y.cadence
 
 chunk :: forall a. Int -> List a -> List (List a)
 chunk 0 _ = Nil
@@ -66,16 +70,18 @@ stripComments :: forall t. { comments :: List Comment | t } -> { comments :: Lis
 stripComments interval = interval { comments = Nil }
 
 collectComments :: List Interval -> List Comment
-collectComments = collectComments' >>> concat where
-    collectComments' :: List Interval -> List (List Comment)
-    collectComments' intervals = zipWith offsetComments (intervalOffsets intervals) (map takeComments intervals)
+collectComments = collectComments' >>> concat
+  where
+  collectComments' :: List Interval -> List (List Comment)
+  collectComments' intervals = zipWith offsetComments (intervalOffsets intervals) (map takeComments intervals)
 
-    offsetComments :: Duration -> List Comment -> List Comment
-    offsetComments amount = map (\ com -> com { offset = com.offset + amount } )
+  offsetComments :: Duration -> List Comment -> List Comment
+  offsetComments amount = map (\com -> com { offset = com.offset + amount })
 
-    -- scanl doesn't produce the first 0-offset - so we prepend 0 manually
-    intervalOffsets :: List Interval -> List Duration
-    intervalOffsets = map takeDuration >>> scanl (+) 0 >>> Cons 0
+  -- scanl doesn't produce the first 0-offset - so we prepend 0 manually
+  intervalOffsets :: List Interval -> List Duration
+  intervalOffsets = map takeDuration >>> scanl (+) 0 >>> Cons 0
 
-    takeComments i = i.comments
-    takeDuration i = i.duration
+  takeComments i = i.comments
+
+  takeDuration i = i.duration
--- src/Workout.purs
+++ src/Workout.purs
@@ -3,29 +3,32 @@ module Zwiftout.Workout where
 import Data.List (List)
 import Data.Maybe (Maybe)
 
-type Workout = {
-  name :: String,
-  author :: String,
-  description :: String,
-  tags :: List String,
-  intervals :: List Interval
-}
+type Workout
+  = { name :: String
+    , author :: String
+    , description :: String
+    , tags :: List String
+    , intervals :: List Interval
+    }
 
-type Interval = {
-  type :: String,
-  duration :: Duration,
-  intensity :: Intensity,
-  cadence :: Maybe Cadence,
-  comments :: List Comment
-}
+type Interval
+  = { type :: String
+    , duration :: Duration
+    , intensity :: Intensity
+    , cadence :: Maybe Cadence
+    , comments :: List Comment
+    }
 
-type Comment = {
-  offset :: Duration,
-  text :: String
-}
+type Comment
+  = { offset :: Duration
+    , text :: String
+    }
 
-type Duration = Int
+type Duration
+  = Int
 
-type Intensity = Number
+type Intensity
+  = Number
 
-type Cadence = Int
+type Cadence
+  = Int
