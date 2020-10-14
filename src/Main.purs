a977544d6b9eec234b9f65fd42e37b5f33ccdfad
Rene Saarsoo <nene@triin.net>
2020-10-14 21:27:30 +0300

    Implement detectRepeats

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,64 @@ module Main where
 import Prelude
 
 import Ast (Comment, Duration, Interval)
-import Data.List (List(..), concat, scanl, zipWith)
+import Data.List (List(..), all, concat, drop, length, scanl, take, takeWhile, zipWith, (:))
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+
+type RepeatedInterval = {
+  times :: Int,
+  intervals :: List Interval,
+  comments :: List Comment
+}
+
+data PlainOrRepeatedInterval = Plain Interval | Repeated RepeatedInterval
+
+instance eqPlainOrRepeatedInterval :: Eq PlainOrRepeatedInterval where
+    eq (Plain a) (Plain b) = a == b
+    eq (Repeated a) (Repeated b) = a == b
+    eq (Plain a) (Repeated b) = false
+    eq (Repeated a) (Plain b) = false
+
+instance showPlainOrRepeatedInterval :: Show PlainOrRepeatedInterval where
+    show (Plain a) = "(Plain " <> show a <> ")"
+    show (Repeated a) = "(Repeated " <> show a <> ")"
+
+windowSize :: Int
+windowSize = 2
+
+detectRepeats :: List Interval -> List PlainOrRepeatedInterval
+detectRepeats intervals = case splitAt windowSize intervals of
+    Tuple Nil _ -> map Plain intervals
+    Tuple _ Nil -> map Plain intervals
+    Tuple (x:xs) ys -> case extractRepeatedInterval (x:xs) ys of
+        Just repeatedInterval -> Repeated repeatedInterval : detectRepeats (drop repetitionLength intervals) where
+            repetitionLength = repeatedInterval.times * windowSize
+        Nothing -> Plain x : detectRepeats (drop 1 intervals)
+
+extractRepeatedInterval :: List Interval -> List Interval -> Maybe RepeatedInterval
+extractRepeatedInterval reference xs = case countRepeats reference xs of
+    0 -> Nothing
+    n -> Just {
+        times: n + 1,
+        intervals: map stripComments reference,
+        comments: collectComments (concat (reference : take (n*windowSize) xs : Nil))
+    }
+
+countRepeats :: List Interval -> List Interval -> Int
+countRepeats reference = chunk (length reference) >>> takeWhile (similar reference) >>> length
+
+similar :: List Interval -> List Interval -> Boolean
+similar xs ys = all identity (zipWith similar' xs ys) where
+    similar' :: Interval -> Interval -> Boolean
+    similar' x y = x.type == y.type && x.duration == y.duration && x.intensity == y.intensity && x.cadence == y.cadence
+
+chunk :: forall a. Int -> List a -> List (List a)
+chunk 0 _ = Nil
+chunk n Nil = Nil
+chunk n xs = take n xs : chunk n (drop n xs)
+
+splitAt :: forall a. Int -> List a -> Tuple (List a) (List a)
+splitAt n xs = Tuple (take n xs) (drop n xs)
 
 stripComments :: forall t. { comments :: List Comment | t } -> { comments :: List Comment | t }
 stripComments interval = interval { comments = Nil }
--- test/Main.purs
+++ test/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Main (collectComments, stripComments)
+import Main (PlainOrRepeatedInterval(..), detectRepeats)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
@@ -13,46 +13,221 @@ import Test.Unit.Main (runTest)
 main :: Effect Unit
 main = do
   runTest do
-    suite "stripComments" do
-      test "nothing happens when no comments to begin with" do
-        Assert.equal { type: "Interval", comments: Nil } (stripComments { type: "Interval", comments: Nil })
-      test "all comments get discarded" do
+    suite "detectRepeats" do
+      test "does nothing with empty list" do
+        Assert.equal Nil (detectRepeats Nil)
+      test "does nothing when no interval repeats" do
         Assert.equal
-          { comments: Nil }
-          (stripComments { comments: { offset: 0, text: "Hello" } : { offset: 20, text: "World" } : Nil })
-    suite "collectComments" do
-      test "Does nothing with empty list" do
-        Assert.equal Nil (collectComments Nil)
-      test "Extracts comments of single item" do
+          (
+            Plain { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : Plain { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : Plain { type: "Interval", duration: 30, intensity: 1.2, cadence: Nothing, comments: Nil }
+            : Plain { type: "Cooldown", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : Nil)
+          (detectRepeats (
+            { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 30, intensity: 1.2, cadence: Nothing, comments: Nil }
+            : { type: "Cooldown", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : Nil))
+
+      test "detects whole workout consisting of repetitions" do
         Assert.equal
-          ({ offset: 0, text: "Hello" } : { offset: 20, text: "World" } : Nil)
-          (collectComments (
-            {
-              type: "Interval",
-              duration: 100,
-              intensity: 1.0,
-              cadence: Nothing,
-              comments: { offset: 0, text: "Hello" } : { offset: 20, text: "World" } : Nil
-            }
+          (
+            (Repeated {
+              times: 4,
+              intervals: (
+                { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+                : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+                : Nil
+              ),
+              comments: Nil
+            }) : Nil
+          )
+          (detectRepeats (
+            { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
             : Nil
           ))
-      test "changes offsets of comments of second interval" do
+
+      test "detects repetitions in the middle of workout" do
         Assert.equal
-          ({ offset: 0, text: "Foo" } : { offset: 100, text: "Hello" } : { offset: 120, text: "World" } : Nil)
-          (collectComments (
+          (
+            Plain { type: "Warmup", duration: 60, intensity: 0.75, cadence: Nothing, comments: Nil }
+            : Plain { type: "Rest", duration: 120, intensity: 0.2, cadence: Nothing, comments: Nil }
+            : Repeated {
+              times: 4,
+              intervals: (
+                { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+                : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+                : Nil
+              ),
+              comments: Nil
+            }
+            : Plain { type: "Rest", duration: 120, intensity: 0.2, cadence: Nothing, comments: Nil }
+            : Plain { type: "Cooldown", duration: 60, intensity: 0.75, cadence: Nothing, comments: Nil }
+            : Nil
+          )
+          (detectRepeats (
+            { type: "Warmup", duration: 60, intensity: 0.75, cadence: Nothing, comments: Nil } -- TODO: use RangeIntensity here
+            : { type: "Rest", duration: 120, intensity: 0.2, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 120, intensity: 0.2, cadence: Nothing, comments: Nil }
+            : { type: "Cooldown", duration: 60, intensity: 0.75, cadence: Nothing, comments: Nil } -- TODO: use RangeIntensity here
+            : Nil
+          ))
+
+      test "detects multiple repetitions" do
+        Assert.equal
+          (
+            Repeated {
+              times: 2,
+              intervals: (
+                { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+                : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+                : Nil
+              ),
+              comments: Nil
+            }
+            : Repeated {
+              times: 2,
+              intervals: (
+                { type: "Interval", duration: 100, intensity: 1.0, cadence: Nothing, comments: Nil }
+                : { type: "Rest", duration: 100, intensity: 0.5, cadence: Nothing, comments: Nil }
+                : Nil
+              ),
+              comments: Nil
+            }
+            : Nil
+          )
+          (detectRepeats (
+            { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 60, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 100, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 100, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 100, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 100, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : Nil
+          ))
+
+      test "takes cadence differences into account" do
+        Assert.equal
+          (
+            Plain { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : Plain { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : Repeated {
+              times: 2,
+              intervals: (
+                { type: "Interval", duration: 120, intensity: 1.0, cadence: Just 100, comments: Nil }
+                : { type: "Rest", duration: 60, intensity: 0.5, cadence: Just 80, comments: Nil }
+                : Nil
+              ),
+              comments: Nil
+            }
+            : Nil
+          )
+          (detectRepeats (
+            { type: "Interval", duration: 120, intensity: 1.0, cadence: Nothing, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Nothing, comments: Nil }
+            : { type: "Interval", duration: 120, intensity: 1.0, cadence: Just 100, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Just 80, comments: Nil }
+            : { type: "Interval", duration: 120, intensity: 1.0, cadence: Just 100, comments: Nil }
+            : { type: "Rest", duration: 60, intensity: 0.5, cadence: Just 80, comments: Nil }
+            : Nil
+          ))
+
+      test "gathers comments together" do
+        Assert.equal
+          (
+            Repeated {
+              times: 2,
+              intervals: (
+                { type: "Interval", duration: 100, intensity: 1.0, cadence: Nothing, comments: Nil }
+                : { type: "Rest", duration: 100, intensity: 0.5, cadence: Nothing, comments: Nil }
+                : Nil
+              ),
+              comments: (
+                { offset: 0, text: "Let's start" }
+                : { offset: 20, text: "Stay strong!" }
+                : { offset: 90, text: "Finish it!" }
+
+                : { offset: 100, text: "Huh... have a rest" }
+                : { offset: 180, text: "Ready for next?" }
+
+                : { offset: 200, text: "Bring it on again!" }
+                : { offset: 250, text: "Half way" }
+                : { offset: 290, text: "Almost there!" }
+
+                : { offset: 330, text: "Wow... you did it!" }
+                : { offset: 340, text: "Nice job." }
+                : { offset: 350, text: "Until next time..." }
+                : Nil
+              )
+            }
+            : Nil
+          )
+          (detectRepeats (
             {
               type: "Interval",
               duration: 100,
               intensity: 1.0,
               cadence: Nothing,
-              comments: { offset: 0, text: "Foo" } : Nil
+              comments: (
+                { offset: 0, text: "Let's start" }
+                : { offset: 20, text: "Stay strong!" }
+                : { offset: 90, text: "Finish it!" }
+                : Nil
+              )
+            }
+            : {
+              type: "Rest",
+              duration: 100,
+              intensity: 0.5,
+              cadence: Nothing,
+              comments: (
+                { offset: 0, text: "Huh... have a rest" }
+                : { offset: 80, text: "Ready for next?" }
+                : Nil
+              )
             }
             : {
               type: "Interval",
-              duration: 10,
+              duration: 100,
               intensity: 1.0,
               cadence: Nothing,
-              comments: { offset: 0, text: "Hello" } : { offset: 20, text: "World" } : Nil
+              comments: (
+                { offset: 0, text: "Bring it on again!" }
+                : { offset: 50, text: "Half way" }
+                : { offset: 90, text: "Almost there!" }
+                : Nil
+              )
+            }
+            : {
+              type: "Rest",
+              duration: 100,
+              intensity: 0.5,
+              cadence: Nothing,
+              comments: (
+                { offset: 30, text: "Wow... you did it!" }
+                : { offset: 40, text: "Nice job." }
+                : { offset: 50, text: "Until next time..." }
+                : Nil
+              )
             }
             : Nil
           ))
